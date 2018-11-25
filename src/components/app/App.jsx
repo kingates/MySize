19c980033edabdf6188b0e5857d97d859a79065d
Kinga Tes <kinga.tes@helpdeskit.local>
2018-11-25 14:40:16 +0100

    Test

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,6 +5,6 @@ import React from "react";
 
 export default class App extends React.Component {
   render() {
-    return <div>To jest app</div>
+    return <div>To jest app TEST TEST TEST</div>
   }
 }
